e93c67be6d154e98605de3b43e760fa5f2b3a433
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2024-02-13 11:22:22 +0200

    fix: Fixed query and changed filter data from ts to json

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+
 import { App } from '@components/App'
 import { ThemeProvider } from '@providers/ThemeProvider'
 
 import './i18n'
+import { queryClient } from './query'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<ThemeProvider>
-			<App />
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider>
+				<App />
+			</ThemeProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 )
