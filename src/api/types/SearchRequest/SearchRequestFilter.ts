3892f34b0536dc0d74c28998ce9395d85ec56db9
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2024-02-25 09:17:04 +0200

    fix: Fix filter search request type

--- src/api/types/SearchRequest/SearchRequestFilter.ts
+++ src/api/types/SearchRequest/SearchRequestFilter.ts
@@ -10,4 +10,4 @@ export interface SearchRequestOptions extends SearchRequestFilterBase {
 	optionsIds: string[]
 }
 
-export type SearchRequestFilter = SearchRequestOptions
+export type SearchRequestFilter = SearchRequestOptions[]
