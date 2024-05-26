630689838a639c368057e2231fd3be83018fa29e
vladyslav5 <vmorshch8@gamil.com>
2024-05-26 16:08:36 +0300

    Added button disabled

--- src/components/FilterModal/ui/FilterModalFooter.tsx
+++ src/components/FilterModal/ui/FilterModalFooter.tsx
@@ -4,12 +4,16 @@ import { useTranslation } from 'react-i18next'
 import { Button, ModalFooter, Stack } from '@chakra-ui/react'
 
 import { ConfirmationModal } from '@components/ConfirmationModal/ConfirmationModal.tsx'
+import useFilter from '@providers/Store/FilterStore.ts'
 
 interface FilterModalFooterProps {
 	onReset: () => void
 }
 export const FilterModalFooter: FC<FilterModalFooterProps> = ({ onReset }) => {
 	const [confirmation, setConfirmation] = useState(false)
+
+	const { filters, oldFilters } = useFilter()
+	const isEqual = JSON.stringify(oldFilters) === JSON.stringify(filters)
 	const onApply = () => {
 		setConfirmation(true)
 	}
@@ -30,6 +34,7 @@ export const FilterModalFooter: FC<FilterModalFooterProps> = ({ onReset }) => {
 				<div></div>
 				<Button
 					color={'#FFFFFF'}
+					isDisabled={isEqual}
 					borderRadius={'16'}
 					px={'70px'}
 					py={'26px'}
